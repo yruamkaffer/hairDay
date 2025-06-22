19da696483948ba72324095ab6b9db3d9b5a016d
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-06-22 15:14:10 +0000

    Add files via upload

---
+++ src/libs/dayjs.js
@@ -0,0 +1,4 @@
+import dayjs from "dayjs";
+import "dayjs/locale/pt-BR";
+
+dayjs.locale("pt-BR");
---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict"
+
+import "./libs/dayjs"
+
+import './styles/global.css';
+import './styles/form.css';
+import './styles/schedule.css';
